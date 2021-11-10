779a4ab2caba87f86953f36dfae54e7938987e91
Isaac Dutton <k1p3psyygojb@nowbuzzoff.com>
2021-11-10 12:13:59 -0500

    Completed hash exercises

---
+++ 12 - Hashes/Multiple_cars.rb
@@ -0,0 +1,13 @@
+vehicles = {
+  Car: {
+  type: 'sedan',
+  color: 'blue',
+  year: 2003
+},
+  Truck: {
+  type: 'pickup',
+  color: 'red',
+  year: 2003
+}}
+
+p vehicles
---
+++ 12 - Hashes/Which_collection.rb
@@ -0,0 +1,3 @@
+
+car = [[type:,'sedan'],[color:,'blue'],[year:,2003]]
+
